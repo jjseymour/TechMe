c385c7667af6ef853c4a3835d99535b83c01c60b
jjseymour <jjseymour@gmail.com>
2016-06-03 16:00:35 -0400

    generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'jobs/index'
+
+  get 'jobs/show'
+
+  get 'events/index'
+
+  get 'events/show'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'home/show'
+
+  get 'registrations/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

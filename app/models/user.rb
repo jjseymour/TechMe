45f4350255d03cc595cf310f8629853df39c9baf
jjseymour <jjseymour@gmail.com>
2016-06-03 15:56:17 -0400

    generated models

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_many :user_events
+  has_many :events, through: :user_events
+  has_many :user_jobs
+  has_many :jobs, through: :user_jobs
+  has_many :user_languages
+  has_many :languages, through: :user_languages
+end
---
+++ db/schema.rb
@@ -0,0 +1,97 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160603194215) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "rsvp"
+    t.string   "location"
+    t.boolean  "paid_event"
+    t.string   "price"
+    t.integer  "members"
+    t.integer  "language_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["language_id"], name: "index_events_on_language_id", using: :btree
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "company"
+    t.string   "position"
+    t.string   "salary"
+    t.string   "date_posted"
+    t.text     "description"
+    t.integer  "language_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["language_id"], name: "index_jobs_on_language_id", using: :btree
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "pic_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_events", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_user_events_on_event_id", using: :btree
+    t.index ["user_id"], name: "index_user_events_on_user_id", using: :btree
+  end
+
+  create_table "user_jobs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "job_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_user_jobs_on_job_id", using: :btree
+    t.index ["user_id"], name: "index_user_jobs_on_user_id", using: :btree
+  end
+
+  create_table "user_languages", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "language_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["language_id"], name: "index_user_languages_on_language_id", using: :btree
+    t.index ["user_id"], name: "index_user_languages_on_user_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.string   "avatar_pic"
+    t.string   "g_access_token"
+    t.string   "g_refresh_token"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_foreign_key "events", "languages"
+  add_foreign_key "jobs", "languages"
+  add_foreign_key "user_events", "events"
+  add_foreign_key "user_events", "users"
+  add_foreign_key "user_jobs", "jobs"
+  add_foreign_key "user_jobs", "users"
+  add_foreign_key "user_languages", "languages"
+  add_foreign_key "user_languages", "users"
+end

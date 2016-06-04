07b1ff051cab20656433479f54bdb352556cfd3d
jjseymour <jjseymour@gmail.com>
2016-06-04 18:09:22 -0400

    authentication done, bro

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,28 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Login Failed, bro"
+      render :login_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    session[:current_org] = nil
+    redirect_to login_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      # binding.pry
+      flash[:alert] = "Thank you for joining TechMe, bro. Your account has be activated. Please login to continue."
+      redirect_to login_path
+    else
+      flash[:alert] = "There was an error with your registration. Please try again."
+      render new_registration_path
+    end
   end
 
   def show
@@ -12,5 +22,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end

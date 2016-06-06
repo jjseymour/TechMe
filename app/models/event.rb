9876aa5a7d380dbbf84ae8310ae8ca35dddf2f68
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-06 11:17:12 -0400

    Set up Meetup API

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,19 @@ class Event < ApplicationRecord
   has_many :user_events
   has_many :users, through: :user_events
   belongs_to :language
+
+  def self.get_events(location, languages)
+    events = []
+
+    languages.each do |e|
+      events << meetup_call(location, e)
+    end
+    events 
+  end
+
+  private
+
+  def self.meetup_call(location, language)
+    JSON.parse(RestClient.get("https://api.meetup.com/find/groups?key=#{ENV["meetup_app_key"]}&sign=true&photo-host=public&country=USA&location=New York&text=ruby &radius=5&fields=ruby&category=34&page=20"))
+  end
 end

56e3e226e15d4e4b9baea1b6307c57e3825eae7f
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-06 09:04:35 -0400

    Basic events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   has_many :user_events
   has_many :users, through: :user_events
   belongs_to :language
+
+  def self.meetup_call
+    RestClient.get("https://api.meetup.com/find/groups?zip=10021")
+  end
 end

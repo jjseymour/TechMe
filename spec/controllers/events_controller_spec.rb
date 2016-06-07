ac8a10de12c6f311a7988963f76866d3371ed36e
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-06 23:17:43 -0400

    Added some red tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 describe EventsController do
 
-  # WE HAVE TO FIGURE CONTROLLER TESTS WITH AUTH ENABLED
+  # WE HAVE TO FIGURE CONTROLLER TESTS WITH AUTH ENABLED: let and before
+  
+  #let(:user) {User.create(username: "Bromance", password: "password")}
+  before(:each) do
+    user = User.new(
+      username: "Bromano", 
+      password: "password1234",
+      password_confirmation: "password1234"
+      )
+    @user = User.find_by(username: "Bromano")
+end
   
   describe "GET #index" do
     it "gets the index view" do

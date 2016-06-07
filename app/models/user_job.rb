b430f341b1d1ce3b183c170ec0e8c870a3919fcf
jjseymour <jjseymour@gmail.com>
2016-06-07 13:30:25 -0400

    fixed navbar and took off validations on join tables

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -2,5 +2,5 @@ class UserJob < ApplicationRecord
   belongs_to :user
   belongs_to :job
 
-  validates_uniqueness_of :user_id, :scope => :job_id
+  # validates_uniqueness_of :user_id, :scope => :job_id
 end

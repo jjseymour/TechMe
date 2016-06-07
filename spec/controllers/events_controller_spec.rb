3d87220adecdc8fb110b4e10042c7bd24353e4e9
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-07 13:32:26 -0400

    Added Controller Tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,22 +1,34 @@
 require 'rails_helper'
 
 describe EventsController do
-
-  # WE HAVE TO FIGURE CONTROLLER TESTS WITH AUTH ENABLED: let and before
-  
+ 
   #let(:user) {User.create(username: "Bromance", password: "password")}
-  before(:each) do
+  before :each do
     user = User.new(
       username: "Bromano", 
       password: "password1234",
       password_confirmation: "password1234"
       )
-    @user = User.find_by(username: "Bromano")
+    event = Event.new(
+      name: "bypassing", 
+      rsvp: false, 
+      location: "Hayleeland, 1241 Johnpaul Groves, 22111-5368", 
+      paid_event: false, 
+      price: "25.45", 
+      members: 19, 
+      language_id: 9
+      )
+
+    fill_in('location', with: 'New York')
+    fill_in('language_selected', with: 'Ruby')
+
+    #binding.pry
+    #@user = User.find_by(username: "Bromano")
 end
   
   describe "GET #index" do
-    it "gets the index view" do
-      get "index"
+    it "gets the events view after user signed up" do
+      get "events"
       expect(response).to render_template :events # :index
     end
   end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe UsersController do
+ 
+  #let(:user) {User.create(username: "Bromance", password: "password", password_confirmation: "password")}
+  before :each do
+    user = User.new(
+      username: "Bromano", 
+      password: "password1234",
+      password_confirmation: "password1234"
+      )
+    event = Event.new(
+      name: "bypassing", 
+      rsvp: false, 
+      location: "Hayleeland, 1241 Johnpaul Groves, 22111-5368", 
+      paid_event: false, 
+      price: "25.45", 
+      members: 19, 
+      language_id: 9
+      )
+
+    within(".form-control") do 
+      fill_in('location', with: 'New York')
+      check('Ruby')
+    end 
+    #binding.pry
+    #@user = User.find_by(username: "Bromano")
+end
+  
+  describe "GET Users#show view page" do
+    it "gets the users show view after user signed up" do
+      get "users"
+      expect(response).to render_template :users
+    end
+  end
+
+end 

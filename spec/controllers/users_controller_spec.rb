67256741dddfe87506e266beb6432d5cddc08005
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-07 15:32:05 -0400

    Controller tests debugging

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,11 +4,14 @@ describe UsersController do
  
   #let(:user) {User.create(username: "Bromance", password: "password", password_confirmation: "password")}
   before :each do
-    user = User.new(
+    visit "/"
+    
+    user = User.create(
       username: "Bromano", 
       password: "password1234",
       password_confirmation: "password1234"
       )
+    
     event = Event.new(
       name: "bypassing", 
       rsvp: false, 
@@ -19,16 +22,21 @@ describe UsersController do
       language_id: 9
       )
 
-    it 'visits the users page', :js => true do
-     visit '/users'
+    click_link('Login')
+    #within("form#new_user.new_user") do 
+      fill_in('Username', with: 'Bromano')
+      fill_in('Password', with: 'password1234')
+      fill_in('Password confirmation', with: 'password1234')
+    #end 
+    #it 'visits the users page', :js => true do
+     visit "/users/#{user.id}"
 
-      within(".form-control") do 
+      within("#event_location") do 
         fill_in('location', with: 'New York')
         check('Ruby')
       end 
-    #binding.pry
     #@user = User.find_by(username: "Bromano")
-    end 
+    #end 
 end
   
   describe "GET Users#show view page" do

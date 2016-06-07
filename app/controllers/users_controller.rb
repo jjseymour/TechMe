909d204aa2772cbe2454ed3b5ee1c3456a50659c
jjseymour <jjseymour@gmail.com>
2016-06-07 18:31:05 -0400

    added errors to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id 
-      flash[:alert] = "Thank you for joining TechMe, bro. Your account has be activated. Please login to continue."
-      redirect_to user_path(@user)
+      session[:user_id] = @user.id
+      # flash[:alert] = "Thank you for joining TechMe, bro. Your account has be activated. Please login to continue."
+      redirect_to user_path(@user), alert: "Thank you for joining TechMe, bro. Your account has be activated. Please login to continue."
     else
-      flash[:alert] = "There was an error with your registration. Please try again."
-      render new_registration_path
+      flash.now[:alert] = "There was an error with your registration. Please try again."
+      render 'registrations/new'
     end
   end
 
